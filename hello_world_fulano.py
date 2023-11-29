a1dd8a297f5947bf932e4b416570bed6e7ad60ac
Larissa Lucena <larissa.lucena@moises.ai>
2023-11-29 11:03:18 -0300

    reorganize

---
+++ hello_world_fulano.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Input do arquivo de texto
+df = pd.read_csv('fulano.txt', header=None, names=['nome'], dtype=str)
+
+# Adcionar constantes
+df['mensagem'] = 'Hello World'
+df['exclamacao'] = '!'
+df['espaco'] = ' '
+
+# Juntar campos
+df['valuename'] = df['mensagem'] + df['espaco'] + df['nome'] + df['exclamacao']
+
+# Salvar arquivo txt
+df['valuename'].to_csv('output.txt', index=False, header=False)
